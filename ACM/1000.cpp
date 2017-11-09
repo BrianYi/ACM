4a521930057dfe300f4b7dfbae25ee8fcd59dde4
BrianYi <yijiazhen123@gmail.com>
2017-11-09 16:43:49 +0800

    first init

---
+++ ACM/1000.cpp
@@ -0,0 +1,23 @@
+// A + B Problem
+// Problem Description
+// Calculate A + B.
+// Input
+// Each line will contain two integers A and B.Process to end of file.
+// Output
+// For each case, output A + B in one line.
+// Sample Input
+// 1 1
+// Sample Output
+// 2
+// DATE: 2017/11/9 BrianYi
+
+// #include <iostream>
+// using namespace std;
+// 
+// int main()
+// {
+//     int a, b;
+//     while (cin >> a >> b)
+//         cout << a + b << endl;
+//     return 0;
+// }
---
+++ README.md
@@ -0,0 +1,4 @@
+# ACM
+---
+<h3 align="center">*************** HDOJ ***************</h3>
+<p> The project is saved all the problem solution that I have resolved.
